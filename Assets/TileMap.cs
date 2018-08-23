8851388287a1e89e78d7af959b44156f2512c8ba
Ferateton <ferateton@hotmail.com>
2018-08-23 10:15:05 +0200

    Add files via upload

---
+++ Assets/TileMap.cs
@@ -0,0 +1,253 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TileMap : MonoBehaviour
+{
+    /*this is the selected object will have to make a function that can make it so one can change 
+    unit that is selected*/
+    public GameObject selectedUnit;
+
+    public TileType[] tileTypes;
+
+    int[,] tiles;
+    Node[,] graph;
+
+    List<Node> CurrentPath = null;
+
+    public int mapSizeX = 10;
+    public int mapSizeY = 10;
+	// Use this for initialization
+	void Start ()
+    {
+        selectedUnit.GetComponent<Unit>().tileX = (int)selectedUnit.transform.position.x;
+        selectedUnit.GetComponent<Unit>().tileY = (int)selectedUnit.transform.position.y;
+        selectedUnit.GetComponent<Unit>().map = this;
+        MapDataGeneration();
+        GeneratePathfindingGraph();
+        GenerateMapVisuals();
+	}
+
+    public float CostToEnterTile(int sourceX, int sourceY,int targetX, int targetY)
+    {
+
+        TileType tt = tileTypes[tiles[targetX, targetY]];
+
+        if(UnitCanEnterTile(targetX,targetY)==false)
+        { return Mathf.Infinity; }
+        float cost = tt.movementCost;
+        if(sourceX!= targetX && sourceY!= targetY)
+        {
+            cost += 0.001f;
+        }
+        return cost;
+    }
+   
+    void GeneratePathfindingGraph()
+    {
+        // initilaize the array;
+        graph = new Node[mapSizeX, mapSizeY];
+        
+        //initialize a node in the each spot in the array
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                graph[x, y] = new Node();
+                graph[x, y].x = x;
+                graph[x, y].y = y;
+            }
+        }
+
+        //now that all nodes exisat calculate thier neighbours;
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                /*we currently have a 4 way connected map, 
+                  * this also works for a 6 way connected map,*/
+                /* if (x > 0)
+                 { graph[x, y].neighbours.Add(graph[x - 1, y]); }
+                 if (x < mapSizeX - 1)
+                 { graph[x, y].neighbours.Add(graph[x + 1, y]);}
+                 if (y > 0)
+                 { graph[x, y].neighbours.Add(graph[x, y - 1]);}
+                 if (y < mapSizeY - 1)
+                 { graph[x, y].neighbours.Add(graph[x, y + 1]);}*/
+
+                if (x > 0)
+                {
+                    graph[x, y].neighbours.Add(graph[x - 1, y]);
+                    if (y > 0)
+                    { graph[x, y].neighbours.Add(graph[x - 1, y-1]); }
+                    if (y < mapSizeY - 1)
+                    { graph[x, y].neighbours.Add(graph[x-1, y + 1]); }
+                }
+                if (x < mapSizeX - 1)
+                {
+                    graph[x, y].neighbours.Add(graph[x + 1, y]);
+                    if (y > 0)
+                    { graph[x, y].neighbours.Add(graph[x + 1, y - 1]); }
+                    if (y < mapSizeY - 1)
+                    { graph[x, y].neighbours.Add(graph[x + 1, y + 1]); }
+                }
+
+                if (y > 0)
+                { graph[x, y].neighbours.Add(graph[x, y - 1]); }
+                if (y < mapSizeY - 1)
+                { graph[x, y].neighbours.Add(graph[x, y + 1]); }
+
+            }
+        }
+        
+    }
+    void MapDataGeneration()
+    {
+
+        tiles = new int[mapSizeX, mapSizeY];
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                tiles[x, y] = 0;
+            }
+        }
+        tiles[2, 3] = 1;
+        tiles[3, 3] = 1;
+        tiles[4, 3] = 1;
+        tiles[3, 4] = 2;
+        tiles[4, 5] = 2;
+        tiles[4, 6] = 2;
+        tiles[4, 7] = 2;
+        tiles[4, 8] = 2;
+
+    }
+    void GenerateMapVisuals()
+    {
+
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                //temporary
+                TileType tt = tileTypes[tiles[x, y]];
+
+
+                GameObject go = (GameObject)Instantiate(tt.tileVisualPrefab, new Vector3(x, y, 0), Quaternion.identity);
+
+                ClickableTile ct = go.GetComponent<ClickableTile>();
+                ct.tileX = x;
+                ct.tileY = y;
+                ct.map = this;
+            }
+        }
+        
+    }
+    public Vector3 TileCoordToWorldCoord(int x, int y)
+    {
+        //this currently places the unit in its new coordinates
+        return new Vector3(x, y, -1);
+    }
+
+    public bool UnitCanEnterTile(int x, int y)
+    {
+        return tileTypes[tiles[x,y]].isWalkable;
+    }
+    public void GeneratePathTo(int x, int y)
+    {
+        //clears out old path
+        selectedUnit.GetComponent<Unit>().currentPath = null;
+
+        if (UnitCanEnterTile(x, y) == false)
+        {
+            // if we clicked the mountains we should jsut return and not be able to
+            return;
+        }
+        //this is the code that moves the unit.
+        // selectedUnit.transform.position = TileCoordToWorldCoord(x, y);
+        Dictionary<Node, float> dist = new Dictionary<Node, float>();
+        Dictionary<Node, Node> prev = new Dictionary<Node, Node>();
+
+        //setup the "Q"-- the list of nodes that havent been chacked yet.
+        List<Node> unvisited = new List<Node>();
+
+        Node source = graph[
+            selectedUnit.GetComponent<Unit>().tileX,
+            selectedUnit.GetComponent<Unit>().tileY];
+
+        Node target = graph[x, y];
+
+        dist[source] = 0;
+        prev[source] = null;
+
+        //v = vertex
+        /* initailize everything to have infitnity distance, since 
+         * we dont know any better right now, also it it is possible 
+         * that some nodes cant be reached from source, 
+         * which would make infinity reasonable value*/
+        foreach (Node v in graph)
+        {
+            if (v != source)
+            {
+                dist[v] = Mathf.Infinity;
+                prev[v] = null;
+            }
+            unvisited.Add(v);
+        }
+        while (unvisited.Count > 0)
+        {
+            // u is going to be the unvisited node with the smallest distance
+
+
+            //u stands for unvisited
+            Node u = null;
+            foreach(Node possibleU in unvisited)
+            {
+                if (u == null || dist[possibleU] < dist[u])
+                {
+                    u = possibleU;
+                }
+            }
+                
+
+            if(u == target)
+            {
+                break;
+            }
+            unvisited.Remove(u);
+            foreach (Node v in u.neighbours)
+            {
+                //float alt = dist[u] + u.DistanceTo(v);
+                float alt = dist[u] + CostToEnterTile(u.x,u.y,v.x,v.y);
+                if (alt < dist[v])
+                {
+                    dist[v] = alt;
+                    prev[v] = u;
+                    //
+                    
+                    
+                    //
+                }
+            }
+        }
+        if (prev[target] == null)
+        {
+
+            return;
+        }
+        List<Node> CurrentPath = new List<Node>();
+        Node curr = target;
+        // step through the " prev" chain and add it to our path
+        while (curr != null)
+        {
+            CurrentPath.Add(curr);
+            curr = prev[curr];
+
+
+        }
+        CurrentPath.Reverse();
+        selectedUnit.GetComponent<Unit>().currentPath = CurrentPath;
+    }
+	
+}
---
+++ Assets/Unit.cs
@@ -0,0 +1,248 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int tileX;
+    public int tileY;
+    public TileMap map;
+    public List<Node> currentPath = null;
+
+    public float moveSpeed = 2;
+    public float canMove;
+    public float remainingMovement = 0;
+    public bool newTileSelected;
+    public GameObject gameobj;
+    public List<GameObject> whatTile;
+
+    public int[,] placeTile;
+
+
+
+    private void Start()
+    {
+        canMove = moveSpeed;
+
+        SetTileforShadow();
+
+    }
+    void SetTileforShadow()
+    {
+        placeTile = new int[map.mapSizeX, map.mapSizeY];
+        for (int x = 0; x < map.mapSizeX; x++)
+        {
+            for (int y = 0; y < map.mapSizeY; y++)
+            {
+                placeTile[x, y] = 0;
+            }
+        }
+    }
+    private void Update()
+    {
+
+        MovementVisual();
+    }
+    void MovementVisual()
+    {
+        if (currentPath != null)
+        {
+            int currNode = 0;
+
+            while (currNode < currentPath.Count - 1)
+            {
+                Vector3 start = map.TileCoordToWorldCoord(currentPath[currNode].x, currentPath[currNode].y);
+                Vector3 end = map.TileCoordToWorldCoord(currentPath[currNode + 1].x, currentPath[currNode + 1].y);
+
+                Debug.DrawLine(start, end, Color.red);
+
+
+
+                currNode++;
+
+            }
+            if (Vector3.Distance(transform.position, map.TileCoordToWorldCoord(tileX, tileY)) < 0.1f)
+            {
+                AdvancePathing();
+            }
+        }
+        if (newTileSelected != true)
+        {
+            return;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, map.TileCoordToWorldCoord(tileX, tileY), 5f * Time.deltaTime);
+    }
+
+    public void WhereCanMove()
+    {
+
+
+        whatTile = new List<GameObject>();
+        for (int x = 0; x < map.mapSizeX; x++)
+        {
+
+
+            for (int y = 0; y < map.mapSizeY; y++)
+            {
+
+
+
+                map.GeneratePathTo(x, y);
+
+                if (currentPath != null)
+                {
+                    int currNode = 0;
+
+
+                    while (currNode < currentPath.Count - 1)
+                    {
+                        Vector3 start = map.TileCoordToWorldCoord(currentPath[currNode].x, currentPath[currNode].y);
+                        Vector3 end = map.TileCoordToWorldCoord(currentPath[currNode + 1].x, currentPath[currNode + 1].y);
+
+                        Debug.DrawLine(start, end, Color.red);
+
+
+
+                        currNode++;
+                        if (canMove >= 0)
+                        {
+
+
+                            //if (currentPath == null)
+                            // return;
+                            remainingMovement = canMove;
+
+                            if (remainingMovement <= 0)
+                            {
+                                break;
+                            }
+
+                            //if (newTileSelected != true)
+                            //  return;
+
+
+                            remainingMovement -= map.CostToEnterTile(currentPath[0].x, currentPath[0].y, currentPath[1].x, currentPath[1].y);
+                            canMove -= map.CostToEnterTile(currentPath[0].x, currentPath[0].y, currentPath[1].x, currentPath[1].y);
+
+                            if (placeTile[currentPath[currNode].x, currentPath[currNode].y] == 0)
+                            {
+                                whatTile.Add((GameObject)Instantiate(gameobj, new Vector3(currentPath[currNode].x, currentPath[currNode].y, -1), Quaternion.identity));
+
+                                placeTile[currentPath[currNode].x, currentPath[currNode].y] = 1;
+                            }
+
+                            // Instantiate(gameobj, new Vector3(currentPath[currNode].x, currentPath[currNode].y, 0), Quaternion.identity);
+
+
+                        }
+
+                    }
+
+                    canMove = moveSpeed;
+                }
+
+            }
+
+
+            currentPath = null;
+        }
+    }
+
+    void AdvancePathing()
+    {
+        if (canMove >= 0)
+        {
+            if (currentPath == null)
+                return;
+            if (remainingMovement <= 0)
+            {
+                return;
+            }
+            if (newTileSelected != true)
+                return;
+            transform.position = map.TileCoordToWorldCoord(tileX, tileY);
+
+            remainingMovement -= map.CostToEnterTile(currentPath[0].x, currentPath[0].y, currentPath[1].x, currentPath[1].y);
+            canMove -= map.CostToEnterTile(currentPath[0].x, currentPath[0].y, currentPath[1].x, currentPath[1].y);
+
+            tileX = currentPath[1].x;
+            tileY = currentPath[1].y;
+
+            currentPath.RemoveAt(0);
+
+            if (currentPath.Count == 1)
+            {
+                currentPath = null;
+
+                remainingMovement = 0;
+            }
+            //when the new shade must be placed
+            if (remainingMovement <= 0)
+            {
+                SetTileforShadow();
+                whatTile.Clear();
+                WhereCanMove();
+                remainingMovement = 0;
+            }
+
+        }
+    }
+
+    public void MoveUnit()
+    {
+        StartCoroutine(MoveTheUnit());
+    }
+    private IEnumerator MoveTheUnit()
+    {
+        newTileSelected = true;
+        if (canMove > 0)
+        {
+            while (currentPath != null && remainingMovement > 0)
+            {
+                AdvancePathing();
+                yield return new WaitForSeconds(1.5f);
+            }
+            remainingMovement = moveSpeed;
+        }
+    }
+    public void ResetMovement()
+    {
+        newTileSelected = false;
+        canMove = moveSpeed;
+
+        //this part is related to the list of gameobjects of tiles that is created so it can be removed later
+        for (int i = 0; i < whatTile.Count; i++)
+        {
+            Destroy(whatTile[i]);
+        }
+        whatTile.Clear();
+
+
+
+
+    }
+    /*  public void MoveToNextTile()
+      {
+          float remainingMovement = moveSpeed;
+          while (remainingMovement > 0)
+          {
+              if (currentPath == null)
+                  return;
+
+              remainingMovement -= map.CostToEnterTile(currentPath[0].x, currentPath[0].y, currentPath[1].x, currentPath[1].y);
+
+              tileX = currentPath[1].x;
+              tileY = currentPath[1].y;
+              transform.position = map.TileCoordToWorldCoord(tileX,tileY);
+
+              currentPath.RemoveAt(0);
+
+              if (currentPath.Count == 1)
+              {
+                  currentPath = null;
+              }
+          }
+      }*/
+
+
+}
